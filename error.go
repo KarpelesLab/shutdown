a0d6caa027c98b6dc7cf5000c294b0660b9bf381
Mark Karpeles <magicaltux@gmail.com>
2024-07-29 16:08:20 +0900

    add method Go()

--- error.go
+++ error.go
@@ -8,3 +8,17 @@ var errCh = make(chan error)
 func Fatalf(format string, args ...any) {
 	errCh <- fmt.Errorf(format, args...)
 }
+
+// Go runs function(s) f in background, and if any of those functions
+// return an error, it'll be handled as a fatal error and cause shutdown
+// to trigger as if Fatalf was called
+func Go(funcs ...func() error) {
+	for _, f := range funcs {
+		go func() {
+			err := f()
+			if err != nil {
+				errCh <- err
+			}
+		}()
+	}
+}
